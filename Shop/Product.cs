56261914884bc4f88ab6a89d83636799e1c9081d
Дмитрий Матохин <argizol@mail.ru>
2023-10-11 00:21:46 +0300

    реализованы классы Product, Shop и MSTests к ним

---
+++ Shop/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace UnitTests.Shop.Shop
+{
+    public class MyProduct
+    {
+        public int Price { get; set; }
+        public string Title { get; set; }
+
+        public MyProduct(int price, string title)
+        {
+            Price = price;
+            Title = title;  
+
+        }
+
+        public override string ToString()
+        {
+            return $"Название = {Title}, Цена = {Price}";
+        }
+
+    }
+}
