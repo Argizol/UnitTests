cd4cea80ad9a03b73cb751349d8b507aa412abfd
Дмитрий Матохин <argizol@mail.ru>
2023-10-20 15:21:10 +0300

    initial branch commit

---
+++ Seminar3/HW3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Seminar3
+{
+    public class HW3
+    {
+        int n {  get; set; }
+        public HW3() { }
+        public static bool EvenOddNumber(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool NumberInInterval(int n)
+        {
+            return default;
+        }
+
+    }
+}
